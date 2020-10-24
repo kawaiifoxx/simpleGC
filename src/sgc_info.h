9556c122f068d66eb3261e06ac9eb58747a87b17
kawaiifoxx <shreyanshg06@gmail.com>
2020-10-24 19:28:05 +0530

    Updated sgc_info.h added few member variables and getter and setter for ref_count, added some docs as well.

--- src/sgc_info.h
+++ src/sgc_info.h
@@ -5,9 +5,30 @@
 #ifndef SIMPLEGC_SGC_INFO_H
 #define SIMPLEGC_SGC_INFO_H
 
+/*
+ * This class stores necessary information for garbage collection,
+ * Each instance of this class will contain memory location
+ * of object to which this sgc_info object is associated with,
+ * and reference count for each memory location that has
+ * been allocated through sgc_ptr. It will also contain information
+ * that whether a ptr is array and if it is its length will also be available.
+ */
 
 template<class T>
 class sgc_info {
+    /*Reference Count of each sgc_ptr reference*/
+    unsigned ref_count;
+    /*Memory location of object*/
+    T *mem_ptr;
+    bool is_array;
+    /*length will be 0 if pointer is not array.*/
+    unsigned length;
+public:
+    /*Getter for ref_count*/
+    unsigned int get_ref_count() const;
+    /*Setter for ref_count*/
+    void set_ref_count(unsigned int refCount);
+
 
 };
 
